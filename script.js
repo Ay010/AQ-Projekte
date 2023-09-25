08f68e06ce3092628ccc11d6f8471ca88d615a4f
Mr.X <aq-computer@outlook.com>
2023-09-25 16:49:05 +0800

    add Menü button 'but ist not working'

---
+++ script.js
@@ -0,0 +1,8 @@
+let navUL = document.getElementById('navi')
+
+let navButton = document.getElementById('nav-Button')
+
+
+navButton.addEventListener('click', ()=> {
+    navUL.classList.toggle('show')
+})
