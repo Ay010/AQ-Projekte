b29e8dafd211fdbec3d614cae8a7b88b8436ce51
Mr.X <aq-computer@outlook.com>
2023-09-26 17:36:17 +0800

    add nav button +style

---
+++ script.js
@@ -0,0 +1,12 @@
+let menü= document.getElementById('menü');
+
+let ulp = document.getElementById('navel')
+
+let navul = document.getElementById('navi');
+
+menü.addEventListener('click', () => {
+    navul.classList.toggle('show')
+    navel.classList.toggle('show')
+
+})
+
