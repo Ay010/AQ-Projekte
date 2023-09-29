f864947bf2d9d093d10ead2b328badc734ca05b0
Mr.X <aq-computer@outlook.com>
2023-09-29 10:23:57 +0800

    add Kontakt site + style, fix nav bug, fix few bugs

--- script.js
+++ script.js
@@ -6,19 +6,19 @@ let navul = document.getElementById('navi');
 
 menü.addEventListener('click', () => {
     navul.classList.toggle('show')
-    navel.classList.toggle('show')
+    ulp.classList.toggle('show')
 
 })
 
 navul.addEventListener('click', () => {
     navul.classList.toggle('show')
-    navel.classList.toggle('show')
+    ulp.classList.toggle('show')
 
 })
 
 navel.addEventListener('click', () => {
     navul.classList.toggle('show')
-    navel.classList.toggle('show')
+    ulp.classList.toggle('show')
 
 })
 
